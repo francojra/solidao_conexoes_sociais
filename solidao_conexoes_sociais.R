0313aa2825aaed26edac879a955526bc58325327
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-13 23:38:15 -0300

    Add files via upload

---
+++ solidao_conexoes_sociais.R
@@ -0,0 +1,24 @@
+
+# Solidão e conexões sociais ------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ------------------------------------------------------------------------------------------------
+# Data: 13/09/22 ------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/social-connections-and-loneliness --------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Pesquisas disponíveis mostram que conexões sociais são importantes para o
+### nosso bem-estar. Ter suporte de família e amigos é importante para a nossa
+### felicidade e saúde, e é também instrumento para nossa habilidade de compartilhar
+### informações, aprender com os outros e obter oportunidades econômicas.
+
+### Nesse artigo, só exploramos dados sobre solidão e conexões sociais em vários
+### países ao longo do tempo, e revisamos evidências disponíveis sobre como e 
+### porquê solidão e conexões sociais afertam nossa saúde e emocional, assim como
+### bem-estar material.
+
+### Apesar do fato de existir uma clara ligação entre conexões sociais e bem-estar
+### mais pesquisas são necessárias para entender mecanismos causais, tamanho
+### de efeitos e mudanças ao longo do tempo.
+
+
+
